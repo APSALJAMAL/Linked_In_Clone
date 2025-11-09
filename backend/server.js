c6b9d1cbf3ced6d67205b20bccde023cb8647365
APSALJAMAL <jamalapsal@gmail.com>
2025-11-09 18:33:02 +0530

    Fix

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,11 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV !== "production") {
 	app.use(
 		cors({
-			origin: "http://localhost:5173",
-			credentials: true,
+		  origin: process.env.CLIENT_URL,
+		  methods: ["GET", "POST", "DELETE", "PUT"],
+		  allowedHeaders: ["Content-Type", "Authorization"],
 		})
-	);
+	  );
 }
 
 app.use(express.json({ limit: "50mb" }));
